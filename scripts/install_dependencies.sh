3d49756c82c71b0f91a810e7497f9326780e19f4
SumithCP <56859063+SumithCP@users.noreply.github.com>
2020-03-18 21:46:49 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 pwd > /tmp/test
-pip install -r requirement.txt
+pip install -r /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/requirement.txt
