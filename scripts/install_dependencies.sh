450880588aaa5472f279b148e4d270d6e90e722d
SumithCP <56859063+SumithCP@users.noreply.github.com>
2020-03-19 09:58:52 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 yum install gcc -y
+yum install gcc72-c++ -y
 pip install -r /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/requirement.txt
