efeb8c39a4ba004f3d0677df711a604f474c1b57
SumithCP <56859063+SumithCP@users.noreply.github.com>
2020-03-19 14:15:43 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 echo "test"
-conda install -c ufechner -c janharms -c mutirri freekitesim
+conda config --set report_errors true
+
+conda install -c ufechner -c janharms -c mutirri freekitesim libopenblas
 
