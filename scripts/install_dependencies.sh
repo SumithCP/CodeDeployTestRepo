019b256fdc5680870db10baf1f8da7a567f4bc78
SumithCP <56859063+SumithCP@users.noreply.github.com>
2020-03-19 13:27:43 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 conda install -c ufechner -c janharms -c mutirri freekitesim
-yum install gcc -y
-yum install gcc72-c++ -y
-pip install -r /opt/codedeploy-agent/deployment-root/$DEPLOYMENT_GROUP_ID/$DEPLOYMENT_ID/deployment-archive/requirement.txt
+
