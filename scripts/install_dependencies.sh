a3e15af23628fae667dd3092954930ca2781bc88
SumithCP <56859063+SumithCP@users.noreply.github.com>
2020-03-21 12:49:16 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "test"
+yum install httpd -y
+service httpd start
 conda config --set report_errors true
-
 conda install -c ufechner -c janharms -c mutirri freekitesim libopenblas
 
